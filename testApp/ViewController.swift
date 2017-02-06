fb28efb5deccc10b765b55e0ce5b891b007a44b3
sagar1729sagar <ssapps.sagar@gmail.com>
2017-02-06 18:56:55 +0530

    Update ViewController.swift

--- testApp/ViewController.swift
+++ testApp/ViewController.swift
@@ -14,6 +14,7 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         print("I have to push this to github")
+        print("It has been pushed")
     }
 
     override func didReceiveMemoryWarning() {
